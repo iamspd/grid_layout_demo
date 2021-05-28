0bf2a3f71f2a45cc53d11abdfc498c3a8da7f2ac
iamspd <iamspd.ai9@gmail.com>
2021-05-28 07:15:25 -0400

    MainActivity - Method onTappedButton implemented

--- app/src/main/java/com/example/gridlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gridlayoutdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonTapped(View view) {
 
-        Log.i(TAG, Integer.toString(view.getId()));
+        int id = view.getId();
+
+        String myId = view.getResources().getResourceEntryName(id);
+
+        Log.i(TAG, myId);
+
+        int resourceId = getResources().getIdentifier(myId, "raw", getPackageName());
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, resourceId);
+        mPlayer.start();
 
     }
 }

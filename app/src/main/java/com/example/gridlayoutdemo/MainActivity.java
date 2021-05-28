85e367087cb2ae89e86919a1d1cdd4e8790526a8
iamspd <iamspd.ai9@gmail.com>
2021-05-28 07:00:50 -0400

    activity_main.xml - Method onButtonTapped added

--- app/src/main/java/com/example/gridlayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/gridlayoutdemo/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.gridlayoutdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonTapped(View view) {
+
+        Log.i(TAG, Integer.toString(view.getId()));
+
     }
 }
